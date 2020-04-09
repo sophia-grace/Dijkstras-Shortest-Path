823f31d9d8cbc9bb2581c4b6499fbd37f4f056e4
soph <sop@sops-MBP.home>
2020-04-09 14:23:45 -0400

    skeleton for graph constructor

--- Graph.java
+++ Graph.java
@@ -14,6 +14,28 @@ public class Graph {
   Graph(String url) {
     //reads graph from url, sets up V, E, edges
 
+    //initialize Attributes
+    V = 0;
+    E = 0;
+    edges = (ArrayList<Edge>[]) new ArrayList[V];
+
+    try {
+      URL urlString = new URL(url);
+      BufferedReader in = new BufferedReader(new InputStreamReader(urlString.openStream()));
+
+      String inputLine;
+      // skip the metadata
+      in.readLine();
+      // read each line in the dataset
+      while ((inputLine = in.readLine()) != null) {
+
+
+      }
+      in.close();
+
+    } catch(Exception IOException) {
+      System.out.println("Unable to access the data.");
+    }
   } //Graph()
 
   //Accessors
@@ -39,19 +61,13 @@ public class Graph {
   } //toString()
 
   public static void main(String[] args) {
-    try {
-      //create a new graph g with the tinyEWD.txt url as argument.
-      URL url = new URL("https://cs.brynmawr.edu/Courses/cs330/spring2020/tinyEWD.txt");
+    //create a new graph g with the tinyEWD.txt url as argument.
+    String url = "https://cs.brynmawr.edu/Courses/cs330/spring2020/tinyEWD.txt";
+    Graph g = new Graph(url);
 
-      //print out the graph, g (using the print methods) (JAVA) System.out.println(g)
+    //print out the graph, g (using the print methods) (JAVA) System.out.println(g)
 
-      //for vertex <- 0 to g.V()-1 do
-      //      print all edges coming out of v (use getAdj())
-
-    }
-    catch(Exception IOException) {
-      System.out.println("Unable to access the data.");
-    }
-
+    //for vertex <- 0 to g.V()-1 do
+    //      print all edges coming out of v (use getAdj())
   } //main()
 } //Graph
